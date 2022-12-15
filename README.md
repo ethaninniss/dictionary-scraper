cd8136f9e25a9d3f5d16a9ef48667ebc116ac137
ethaninniss <ethan.inniss@gmail.com>
2022-12-15 00:47:48 +0000

    added cards for each word

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Then take this input and take each word and input it into an array
 For each word get data about this word from a dictionary
 Is it a noun, verb, etc.
 Print this data on the screen
+Create cards to display data for each word
+Inside the async function create an eventlistener that creates a new card for when the function is ran
--- script.js
+++ script.js
@@ -14,13 +14,41 @@ const splitSentence = () => {
 
 
 
-
 async function wordFetch(word) {
     let res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     let data = await res.json();
     let todoData = await data;
     console.log(todoData);
+    makeCard(todoData);
 }
 
 
-
+const makeCard = function(arr) {
+    // const element = Array.from(document.getElementsByClassName("card-output"));
+    // element.forEach(x => x.remove());
+    
+    // card
+    const cardContainer = document.getElementById("card-output");
+    let card = document.createElement("div");
+    cardContainer.appendChild(card);
+    card.setAttribute("class", "card");
+    // document.getElementsByClassName("card").border = "1px solid red";
+    
+    // word
+    let word = document.createElement("h3");
+    word.innerHTML = arr[0].word;
+    word.setAttribute("class", "word");
+    card.appendChild(word);
+    
+    // pronunciation
+    let pronunciation = document.createElement("p");
+    pronunciation.innerHTML = arr[0].phonetic;
+    pronunciation.setAttribute("class", "pronunciation");
+    card.appendChild(pronunciation);
+    
+    // definition
+    let definition = document.createElement("p");
+    definition.innerHTML = arr[0].meanings[0].definitions[0].definition;
+    definition.setAttribute("class", "definition");
+    card.appendChild(definition);
+};
